1433e2650f79af326baaf9fe27b7a8f78bdcb8bc
felipe.ricardi <felipe@indexlabs.com.br>
2019-05-17 09:33:00 -0300

    Adding server entrypoint, readfilesync dependency and a example of how to read the line with this module :sparkles: :heavy_plus_sign:

---
+++ server.js
@@ -0,0 +1,8 @@
+var rl = require('readline-sync');
+ 
+// Asking the user for a word
+var word = rl.question('Type a word !\n');
+
+// Showing the word
+console.log('The word you choose was: ' + word + '!\n');
+ 
