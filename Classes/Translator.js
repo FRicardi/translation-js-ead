dcfe2ff7d38721e72ab2d7e7021579f7701a4c57
joaodacolsoares <joao.dacol.soares@gmail.com>
2019-05-19 19:42:01 -0300

    Added the file read in the project

--- Classes/Translator.js
+++ Classes/Translator.js
@@ -1,15 +1,44 @@
+const fileSystem = require('fs');
+const readline = require('readline');  
+
+const Dictionary = require('./Dictionary')
+const LinkedList = require('./LinkedList')
+const AVL = require('./AVL')
+
 /**
  * This class will do the magic of translation
  * @params { AVL } dictionaryTree
  */
 class Translator {
 
+    constructor () {
+        this.avl = new AVL();
+    }
+
     /**
      * Load the file dicionario.dat
      * @param { String } file 
      */
-    loadDictionary(file) {
+    loadDictionary(path) {
+        const readInterface = readline.createInterface({  
+            input: fileSystem.createReadStream(path),
+        });
+        
+        readInterface.on('line', (line) => {
+            const words = line.split('#');
+            const linkedList = new LinkedList();
 
+            for (let i = 1; i < words.length; i++) {
+                linkedList.insertAtEnd(words[i]);
+            }
+
+            const dictionary = new Dictionary(words[0],linkedList);
+            this.avl.insert({dictionary})
+        });
+
+        setTimeout(() => {
+            this.avl.readInOrder();
+        }, 500)
     }
 
     /**
